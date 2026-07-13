d8b0d44bfea9d795d32ffa47084e0ea8a7e8804f
VB <bavi1776ktor@gmail.com>
2026-07-13 09:25:58 +0300

    Create HearingAidEnginePackage.kt

---
+++ modules/hearing-aid-engine/android/src/main/java/com/hearingaid/HearingAidEnginePackage.kt
@@ -0,0 +1,11 @@
+package com.hearingaid
+
+import android.content.Context
+import expo.modules.core.BasePackage
+import expo.modules.core.ExportedModule
+
+class HearingAidEnginePackage : BasePackage() {
+    override fun createExportedModules(context: Context): List<ExportedModule> {
+        return emptyList()
+    }
+}
