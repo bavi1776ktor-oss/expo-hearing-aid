64d5e14885448655054c82c36b5bf91b21109e34
VB <bavi1776ktor@gmail.com>
2026-07-13 13:20:53 +0300

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,10 @@
 const { getDefaultConfig } = require('expo/config');
+// Подключаем официальный метод создания списка исключений для Metro
+const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 const config = getDefaultConfig(__dirname);
 
-// Заставляем Metro полностью игнорировать папку modules при сборке JS
-config.resolver.blacklistRE = /modules\/.*/;
+// Корректно исключаем папку modules из JS-бандлинга
+config.resolver.blacklistRE = exclusionList([/modules\/.*/]);
 
 module.exports = config;
