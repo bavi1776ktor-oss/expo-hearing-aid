6785b3750b90ea6e53e8ddb65cc3f19321402dd8
VB <bavi1776ktor@gmail.com>
2026-07-13 08:54:28 +0300

    Create HearingAidEngineModule.kt

---
+++ modules/hearing-aid-engine/android/src/main/java/HearingAidEngineModule.kt
@@ -0,0 +1,25 @@
+import expo.modules.kotlin.modules.Module
+import expo.modules.kotlin.modules.ModuleDefinition
+
+class HearingAidEngineModule : Module() {
+    companion object {
+        init {
+            System.loadLibrary("hearing_aid_engine")
+        }
+    }
+
+    override fun definition() = ModuleDefinition {
+        Name("HearingAidEngine")
+
+        Function("start") {
+            startEngine()
+        }
+
+        Function("stop") {
+            stopEngine()
+        }
+    }
+
+    private external fun startEngine()
+    private external fun stopEngine()
+}
